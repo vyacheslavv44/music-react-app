368ab26af6a15da093497356bdf5e95ed8167243
vyacheslavv44 <follow.trend7777@gmail.com>
2018-02-11 04:49:18 +0400

    fixed error addTrackBtn undefined

--- src/index.js
+++ src/index.js
@@ -7,59 +7,55 @@ import redux from 'react-redux';//tried here 'redux'
 
 import { Router, Route, Link, browserHistory } from 'react-router-3';
 import Routes from './routes';
-import { createStore } from 'redux';
-
-ReactDOM.render(
-    <Routes history={browserHistory} />,
-    document.getElementById('root')
-);
-
-
-function playlist(state = [], action) {
-    if(action.type === 'ADD_TRACK') {
-        return[
-            ...state, action.payload
-        ];
-    }
-    return state;
-  }
+//import { createStore } from 'redux';
 
 
 
-const store = createStore(playlist);
 
-const addTrackBtn = document.querySelectorAll('.addTrack')[0];
-const trackInput = document.querySelectorAll('.trackInput')[0];
-const list = document.querySelectorAll('.list')[0];
+// function playlist(state = [], action) {
+//     if(action.type === 'ADD_TRACK') {
+//         return[
+//             ...state, action.payload
+//         ];
+//     }
+//     return state;
+//   }
+
+
+
+// const store = createStore(playlist);
+
+// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+// const trackInput = document.querySelectorAll('.trackInput')[0];
+// const list = document.querySelectorAll('.list')[0];
 
 //console.log(addTrackBtn);
 
-store.subscribe(() => {
-    //console.log('subscribe', store.getState());
-    // without it in our list adding (not one at a time) but
-    // all items in array with every button's hit.
-    list.innerHTML = '';
-    //clearing input text field
-    trackInput.value = '';
-    store.getState().forEach(track => {
-        const li = document.createElement('li');
-        li.textContent = track;
-        list.appendChild(li);
-    });
-  })
+// store.subscribe(() => {
+//     //console.log('subscribe', store.getState());
+//     // without it in our list adding (not one at a time) but
+//     // all items in array with every button's hit.
+//     list.innerHTML = '';
+//     //clearing input text field
+//     trackInput.value = '';
+//     store.getState().forEach(track => {
+//         const li = document.createElement('li');
+//         li.textContent = track;
+//         list.appendChild(li);
+//     });
+//   })
 
 
 
 
-addTrackBtn.addEventListener('click', () => {
+/* addTrackBtn.addEventListener('click', () => {
     const trackName = trackInput.value;
     //check for trackName NOT empty
     if (!trackName) {
         return
-      }
-    store.dispatch({ type: 'ADD_TRACK', payload: trackName});
-  });
-
+    }
+    store.dispatch({ type: 'ADD_TRACK', payload: trackName });
+}); */
 
 
   
@@ -67,3 +63,7 @@ addTrackBtn.addEventListener('click', () => {
 
 //registerServiceWorker();
 
+ReactDOM.render(
+    <Routes history={browserHistory} />,
+    document.getElementById('root')
+);
--- src/pages/Tracks/Tracks.js
+++ src/pages/Tracks/Tracks.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './Tracks.css';
+import { createStore } from 'redux';
 
+const store = createStore(playlist);
 
+const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+const trackInput = document.querySelectorAll('.trackInput')[0];
+const list = document.querySelectorAll('.list')[0];
 
 
 class Tracks extends Component {
+  
+  handleClick() {
+    //console.log('this is:', this);
+    const trackName = trackInput.value;
+    store.dispatch = { type: 'ADD_TRACK', payload: trackName }
+  }
   render() {
     return (
       <div className="Tracks">
         <h1>Песни</h1>
 
         <input type="text" placeholder="введите трэк" required class="trackInput" />
-    <button class="addTrack">Добавить</button>
-    <ul class="list">
 
-    </ul>
+        
+        <button class="addTrack" onClick={(e) => this.handleClick(e)}>Добавить</button>
+        <ul class="list">
+
+        </ul>
 
 
     
@@ -24,6 +37,30 @@ class Tracks extends Component {
       </div>
     );
   }
+}
+
+store.subscribe(() => {
+  //console.log('subscribe', store.getState());
+  // without it in our list adding (not one at a time) but
+  // all items in array with every button's hit.
+  list.innerHTML = '';
+  //clearing input text field
+  trackInput.value = '';
+  store.getState().forEach(track => {
+    const li = document.createElement('li');
+    li.textContent = track;
+    list.appendChild(li);
+  });
+})
+
+
+function playlist(state = [], action) {
+  if (action.type === 'ADD_TRACK') {
+    return [
+      ...state, action.payload
+    ];
+  }
+  return state;
 }
 
 
