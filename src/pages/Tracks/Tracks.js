a3a6b61e931bd1160b8c48ef5773d788f48070ca
vyacheslavv44 <follow.trend7777@gmail.com>
2018-03-04 21:01:26 +0400

    used BIG YChebotaev correction

--- src/pages/Tracks/Tracks.js
+++ src/pages/Tracks/Tracks.js
@@ -3,23 +3,22 @@ import { connect } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './Tracks.css';
 import { createStore } from 'redux';
+import { addTrack } from '../../store/reducers/playlist';
 
-const store = createStore(playlist);
+const stateMapper = ({ playlist: tracks }) => ({ tracks });
 
-const addTrack = trackName => ({
-type: 'ADD_TRACK',
-  payload: trackName
-  })
-
-// FIXME [YC]: Здесь я бы я еще улучшил, в store хранил бы такой объект: {tracks: []},
-// чтобы оставить возможность добавлять другие ключи.
-const stateMapper = tracks => ({ tracks });
 
 class Tracks extends Component {
   
   state = {
  trackName: ''
   };
+
+
+  constructor(props) {
+        super(props);
+        Object.assign(this.state, props);
+      }
 
     componentWillReceiveProps({ trackName }) {
       this.setState({ trackName });
@@ -29,6 +28,12 @@ class Tracks extends Component {
         const { trackName } = this.state;
         this.props.addTrack(trackName);
   }
+
+
+  onNameChange = e => {
+      const trackName = e.target.value;
+        this.setState({ trackName })
+        };
 
     renderTrack = trackName => <li>{trackName}</li>
  
